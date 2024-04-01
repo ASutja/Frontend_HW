80c4d569fa54cf4be976181060864b925d643fbd
ASutja <annasutyagina2023@gmail.com>
2024-04-01 23:48:56 +0300

    Добавление файлов из папки HW_2

---
+++ HW_2/.vscode/script.js
@@ -0,0 +1,5 @@
+let name = prompt('What is your name?');
+let food = prompt('What is your favorite food');
+let drink = prompt('What is your favorite drink?');
+
+alert(`Great! ${name} will eat ${food} and drink some ${drink} !`);
