a8e907d389a92b169596c530580f34123ab2349e
ASutja <annasutyagina2023@gmail.com>
2024-04-05 16:13:29 +0300

    Add files from folder HW_8

---
+++ HW_8/script.js
@@ -0,0 +1,8 @@
+let name = prompt('What is your name?');
+
+    if(name !== null){
+        alert(`Hello, ${name}! How are you?`);
+    }
+    else {
+        alert('You did`t enter your name.');
+    }
